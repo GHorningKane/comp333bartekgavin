a495903908e43b4e78e8864cd243ceb1c944acaa
GHorningKane <ghorningkane@wesleyan.edu>
2023-10-14 15:09:50 -0400

    added a config, haven't integrated to previous files. Sign-up.html takes user input, sign-up.php confirms valid input, hashes passwords, and takes you to addsong.html

---
+++ hw2/sign-up.php
@@ -0,0 +1,54 @@
+<?php
+require_once "config.php";
+
+$username = trim($_POST['username']);
+$password = trim($_POST['password']);
+$password_check = trim($_POST['password_check']);
+$input_error = "";      //gather vars from user, input is non-empty if an error has occured.
+
+if(empty($password_check)){ $input_error = "Password confirm empty";   }
+if(empty($password)){    $input_error = "Password empty"; } 
+if(empty($input_error) && ($password != $password_check)){            $input_error = "Password don't match";        }  
+$password = password_hash($password, PASSWORD_DEFAULT);     // have to do this after I compare password and confirm password I guess haha?
+
+if(empty($username)){
+    $input_error = "Username empty";
+} else{
+    $sql = "SELECT username FROM users WHERE username = ?";
+    if($stmt = mysqli_prepare($conn, $sql)){
+        mysqli_stmt_bind_param($stmt, "s", $username);
+        if(mysqli_stmt_execute($stmt)){
+            mysqli_stmt_store_result($stmt);
+            if(mysqli_stmt_num_rows($stmt) == 0){
+                                    $username = $username;
+            } else{               
+                $input_error = "oof";      
+                            }
+        } else{
+            echo "Uh oh, it seems sql wasn't able to execute the statement? Perhaps try again.";
+        }
+    }
+    mysqli_stmt_close($stmt);
+}
+
+echo $input_error;
+
+if(empty($input_error)){
+    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    if($stmt = mysqli_prepare($conn, $sql)){
+        mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+        if(mysqli_stmt_execute($stmt)){
+            header("location: addsong.html");
+        } else{
+            echo "Uh oh, it seems sql wasn't able to execute the statement? Perhaps try again.";
+        }
+    }
+    mysqli_stmt_close($stmt);
+}
+mysqli_close($conn);
+?>
+
+
+
+
+
