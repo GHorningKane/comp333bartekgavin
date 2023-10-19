824c32b2862dd9fe9f9a80daa1eb6528150ca842
Ian Reyes <ianreyes@Ians-MacBook-Pro-2.local>
2023-10-19 00:42:28 -0400

    Password Requirements

--- hw2/sign-up.php
+++ hw2/sign-up.php
@@ -6,9 +6,11 @@ $password = trim($_POST['password']);
 $password_check = trim($_POST['password_check']);
 $input_error = "";      //gather vars from user, input is non-empty if an error has occured.
 
-if(empty($password_check)){ $input_error = "Password confirm empty"; echo $input_error;  }
-if(empty($password)){    $input_error = "Password empty"; echo $input_error;} 
-if(empty($input_error) && ($password != $password_check)){            $input_error = "Password don't match"; echo $input_error;        }  
+if(empty($password_check)){ $input_error = "Password confirm empty<br>"; echo $input_error;  }
+if(empty($password)){    $input_error = "Password empty<br>"; echo $input_error;} 
+if(strlen($password) < 10) { $input_error = "Password must be at least 10 characters long<br>"; echo $input_error;}
+if(!preg_match("/[0-9]/", $password)) {$input_error = "Password must contain at least 1 number<br>"; echo $input_error;}
+if(empty($input_error) && ($password != $password_check)){            $input_error = "Password don't match<br>"; echo $input_error;        }  
 $password = password_hash($password, PASSWORD_DEFAULT);     // have to do this after I compare password and confirm password I guess haha?
 
 if(empty($username)){
